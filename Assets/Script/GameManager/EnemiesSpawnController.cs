b4e762c19127f7912eaf5b3f154823b42aa4d074
SlayerWx <maxi_cs_95@hotmail.com>
2021-12-05 06:27:37 -0300

    map implemented

---
+++ Assets/Script/GameManager/EnemiesSpawnController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesSpawnController : MonoBehaviour
+{
+    public Transform[] spawners;
+    public GameObject groundEnemy;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
