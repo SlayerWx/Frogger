1034a50e4e6929ea56747d140b9b419b7920f59c
SlayerWx <maxi_cs_95@hotmail.com>
2021-12-04 22:50:57 -0300

    basic move with animation

---
+++ Assets/Script/Player/AnimatorController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public enum AnimationState
+{
+    idle,front,right,back,left
+}
+public class AnimatorController : MonoBehaviour
+{
+    public Animator myAnim;
+    private void OnEnable()
+    {
+        PlayerMove.OnMove += Play;
+    }
+    private void OnDisable()
+    {
+
+        PlayerMove.OnMove -= Play;
+    }
+    public void Play(AnimationState state)
+    {
+        if(state != AnimationState.idle)
+        {
+            myAnim.SetFloat("blendidle",(int)state);
+        }
+        myAnim.SetInteger("direction",(int)state);
+    }
+}
---
+++ Assets/Script/Player/PlayerMove.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class PlayerMove : MonoBehaviour
+{
+    public static Action<AnimationState> OnMove;
+    private bool moving = false;
+    private bool dead = false;
+    public float speed = 1.0f;
+    public float distance = 1.0f;
+    void Start()
+    {
+        dead = false;
+        moving = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerInput();
+    }
+    void PlayerInput()
+    {
+        if (!moving)
+        {
+            if (Input.GetAxis("Horizontal") < 0)
+            {
+                StartCoroutine(MoveToDirection(Vector3.left, AnimationState.left));
+            }
+            if (Input.GetAxis("Horizontal") > 0)
+            {
+                StartCoroutine(MoveToDirection(Vector3.right, AnimationState.right));
+            }
+            if (Input.GetAxis("Vertical") < 0)
+            {
+                StartCoroutine(MoveToDirection(Vector3.down, AnimationState.back));
+            }
+            if (Input.GetAxis("Vertical") > 0)
+            {
+                StartCoroutine(MoveToDirection(Vector3.up,AnimationState.front));
+            }
+        }
+    }
+    IEnumerator MoveToDirection(Vector3 direction, AnimationState newANim)
+    {
+        float timer = 0.0f;
+        moving = true;
+        OnMove?.Invoke(newANim);
+        Vector3 startPosition = transform.position;
+        while (moving)
+        {
+            yield return null;
+            timer += speed * Time.deltaTime;
+            if (timer > distance)
+            {
+                timer = distance;
+                moving = false;
+            }
+            transform.position = Vector2.Lerp(startPosition, startPosition + (direction * distance), timer / distance);
+        }
+        OnMove?.Invoke(AnimationState.idle);
+    }
+}
