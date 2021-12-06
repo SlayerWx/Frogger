5f29efcdbe6dde0a11713be98f1e143c4a4aff37
SlayerWx <maxi_cs_95@hotmail.com>
2021-12-06 01:07:59 -0300

    splashscreen

---
+++ Assets/Script/SplashScreen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    // asAS
+    [SerializeField] string nextScene = null;
+    [SerializeField] float time = 0;
+    public GameObject layer1;
+    public GameObject layer2;
+    void Start()
+    {
+        StartCoroutine(Timer());
+    }
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(time);
+        layer1.SetActive(true);
+        yield return new WaitForSeconds(time);
+        layer1.SetActive(false);
+        layer2.SetActive(true);
+        yield return new WaitForSeconds(time);
+        SceneManager.LoadScene(nextScene);
+    }
+}
